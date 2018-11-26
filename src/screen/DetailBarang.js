f2939f5575011ffdbf3fcecfa02462e285d206eb
sodojiwo <alvinadetya@rocketmail.com>
2018-11-27 05:58:27 +0700

    crud barang

--- src/screen/DetailBarang.js
+++ src/screen/DetailBarang.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
-
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from 'reactstrap';
 export default class DetailBarang extends Component {
+  state = {
+    detailItem: {}
+  };
+  componentDidMount() {
+    let barang = JSON.parse(localStorage.getItem('barang'));
+    let getDetailItem = barang[this.props.match.params.id];
+    this.setState({
+      detailItem: getDetailItem
+    });
+  }
   render() {
+    const { nama, harga, url } = this.state.detailItem;
     return (
       <div>
-        <h1>Detail Barang</h1>
+        <Card style={{ height: 400 }}>
+          <CardImg top width="100%" src={url} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{nama}</CardTitle>
+            <CardSubtitle>Harga Rp {harga}</CardSubtitle>
+            <CardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </CardText>
+            <Button>Beli</Button>
+            <Button>Batal</Button>
+          </CardBody>
+        </Card>
       </div>
     );
   }
--- src/screen/ListBarang.js
+++ src/screen/ListBarang.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import { Table, Button } from 'reactstrap';
-
+import { Link } from 'react-router-dom';
 export default class ListBarang extends Component {
+  state = {
+    barang: [],
+    transactionItems: []
+  };
+  handleBeli = e => {
+    const { transactionItems } = this.state;
+    const beli = this.state.barang[e];
+    transactionItems.push(beli);
+    localStorage.setItem('transaction', JSON.stringify(transactionItems));
+  };
+  componentDidMount() {
+    this.setState({
+      barang: this.props.barang
+    });
+  }
   render() {
     return (
       <div>
@@ -11,7 +26,6 @@ export default class ListBarang extends Component {
               <th>#</th>
               <th>Nama Barang</th>
               <th>Harga</th>
-              <th>Gambar</th>
             </tr>
           </thead>
           <tbody>
@@ -21,9 +35,11 @@ export default class ListBarang extends Component {
                   <th scope="row">{key + 1}</th>
                   <td>{datum.nama}</td>
                   <td>{datum.harga}</td>
-                  <td>{datum.url}</td>
+
                   <td>
-                    <Button>Beli</Button>
+                    <Link to={`/barang/${key}`}>
+                      <Button>Detail Makanan</Button>
+                    </Link>
                   </td>
                 </tr>
               );
